5530b6ae7a574296d8434dc15a91232bc3d1bedb
Rakeshkumar Yadav <yyyrakesh@gmail.com>
2020-07-22 19:09:41 +0530

    master - Adding project into repo.

---
+++ springboot_jpa_map/src/main/java/com/rakesh/jpamap/SpringbootJpaMapApplication.java
@@ -0,0 +1,13 @@
+package com.rakesh.jpamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaMapApplication.class, args);
+	}
+
+}
---
+++ springboot_jpa_map/src/test/java/com/rakesh/jpamap/SpringbootJpaMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.jpamap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
